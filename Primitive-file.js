ab5a6563cd1c950cccbfcc4b3fe4415cb7645c23
Jacob Palombaro <jacob.palombaro@gmail.com>
2021-11-22 12:35:27 -0600

    update

---
+++ Primitive-file.js
@@ -0,0 +1,10 @@
+console.log(typeof 99.9);
+console.log(typeof "false")
+console.log(typeof false);
+console.log(typeof true);
+console.log(typeof 'true')
+
+
+
+
+
---
+++ strings.js
@@ -0,0 +1,20 @@
+var myName = "Jacob.";
+
+var aSentence = "Hi " + "my " + "name " + "is " + myName;
+
+console.log(aSentence);
+
+var aSentence = "I currently work full time at Lackland while attending this course, and have a 10 month old daughter.";
+
+console.log(aSentence);
+
+var aSentence = "I will be relocating up to Buffalo NY after graduation.";
+
+console.log(aSentence);
+
+
+
+
+
+
+
