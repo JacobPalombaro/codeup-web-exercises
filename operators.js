901014a32b1a385988d0c389a89e5ceddbc259d3
Jacob Palombaro <jacob.palombaro@gmail.com>
2021-11-22 12:59:27 -0600

    update

---
+++ operators.js
@@ -0,0 +1,29 @@
+console.log('1' + 2);
+
+console.log(typeof '1' + 2 );
+
+console.log(6 % 4);
+
+console.log('3 + 4 is ' + 3+ 4);
+
+console.log(0 < 0);
+
+console.log('false' == false);
+
+console.log(5 >= -5);
+
+console.log(!false || false);
+
+console.log(6 % 5);
+
+console.log(5 < 4 && 1 === 1);
+
+console.log(typeof 'codeup' === 'string');
+
+console.log('codeup'=== 'codeup' && 'codeup' === 'Codeup');
+
+console.log(4 >= 0 && 1 !== '1');
+
+console.log(6 % 3 === 0);
+
+console.log(5 % 2 !== 0);
