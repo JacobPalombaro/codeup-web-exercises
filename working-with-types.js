fa62691c5c64577b0b93cc0ba5ecef2c2af40a2b
Jacob Palombaro <jacob.palombaro@gmail.com>
2021-11-23 10:25:15 -0600

    update

--- working-with-types.js
+++ working-with-types.js
@@ -1,18 +1,26 @@
-// what is the value of a, b, and c?
-
-var a = 1;
-console.log(a);
-
-var b = a++;
-console.log(b);
-
-var c = ++a;
-console.log (c);
 
 
-//var a = 1;
-//var b = a++;
-//var c = ++a;
-// what is the value of a, b, and c?
+let littleMermaid = 3
+let brotherBear = 5
+let hercules = 1
+console.log((littleMermaid + brotherBear + hercules) * 3)
+
+var payGoogle = 400
+var payAmazon = 380
+var payFacebook = 350
+console.log((payGoogle * 6) + (payFacebook * 10) + (payAmazon * 4));
+
+var classroom = "not full"
+var schedule = "no conflicts"
+if ((classroom = "not full") && (schedule = "no conflitcs")){
+    console.log("student may enroll")
+}
+
+var username ="codeup";
+var password = 'notastrongpassword';
+console.log(password.length >= 5);
+console.log(password == password.replace("codeup" , ""));
+console.log(username.length <= 20);
+console.log(username == username.trim()) && (password ==.trim());
 
 
