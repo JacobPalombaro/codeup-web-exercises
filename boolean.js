2975e7e08c91d8c0c7ba9e792fbcc6af846ccc69
Jacob Palombaro <jacob.palombaro@gmail.com>
2021-11-23 09:15:47 -0600

    update

---
+++ boolean.js
@@ -0,0 +1,12 @@
+
+
+// Determine if a person can buy a drink at a bar
+//are they at the bar?
+//are they sober?
+//are they 21?
+//Do they have enough money?
+
+let isAtBar = false;
+let bac = .07;
+let age = 22;
+let cashOnHand = 7.32;
