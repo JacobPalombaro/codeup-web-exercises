4b640a59e2d79eb122043d2c550a07303a90ba4a
Jacob Palombaro <jacob.palombaro@gmail.com>
2021-11-23 09:48:03 -0600

    update

--- boolean.js
+++ boolean.js
@@ -1,12 +1,19 @@
-
-
 // Determine if a person can buy a drink at a bar
 //are they at the bar?
 //are they sober?
 //are they 21?
 //Do they have enough money?
 
-let isAtBar = false;
+let isAtBar = true;
 let bac = .07;
 let age = 22;
-let cashOnHand = 7.32;
+let cashOnHand = 5.99;
+
+
+let canHaveADrink = (isAtBar || (bac < .08 && age >= 21 && cashOnHand >= 5.00))  // true && false && true && true --> false
+
+if (canHaveADrink) { // true or false
+    console.log("have a brewski my dude!");
+} else { // it was not true
+    console.log("sorry buddy, no brew for you");
+}
