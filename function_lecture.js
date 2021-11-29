45e1459ccd6dc16a03ae4fbc3e5e62c237123c81
Jacob Palombaro <jacob.palombaro@gmail.com>
2021-11-29 10:34:52 -0500

    update

---
+++ function_lecture.js
@@ -0,0 +1,10 @@
+var isAGoodDog = true;
+
+function tellMeIfTheDoggoWasGood(){
+    let isAGoodDog = true;
+    return isAGoodDog
+    ? "Such a good boy!"
+    : "Eh, he's had better days";
+}
+
+console.log(tellMeIfTheDoggoWasGood());
