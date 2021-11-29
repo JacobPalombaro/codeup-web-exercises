5a668c5b8bc407741568a6bc347e3b16f8a6f326
Jacob Palombaro <jacob.palombaro@gmail.com>
2021-11-29 12:37:40 -0500

    update

---
+++ intro.js/conditionals.js
@@ -0,0 +1,62 @@
+"use strict";
+// MORNING WARMUP:...
+
+
+
+// suny --> shirt
+
+
+// if statement:
+// if weather is sunny, wear shirt
+// let weather = "snowy";
+//
+//
+//     if (weather === "sunny") {
+//         console.log("wear shirt");
+//     }   else if (weather === "rainy") {
+//         console.log("bring umbrella");
+//     }else if ( weather === "snowy"){
+// console.log("wear 10 jackets")
+//     }
+//     else {
+//         console.log("wear whatever");
+//     }
+    //
+// let myAge = 21;
+// let actualAge = 31;
+//
+//     function checkAge (givenAge, realAge){
+//         console.log(givenAge);
+//         console.log(myAge);
+//         if (givenAge === realAge){
+//             console.log("myAge is my actual age")
+//         }
+//     }
+//     checkAge(myAge, actualAge);
+
+// Create 5 different variables that holds 5 different number grades.
+// (ex: let laurasGrade = 99) Write a function that accepts a student’s number grade,
+// and returns  their letter grade based on their number grade. Call the
+// function for each student.
+// Scoring: A: 90 + , B: 80 - 89, C: 70-79, D: 60 - 69, F: 60 - (edited)
+
+function letterGrade (studentGrade) {
+    if (studentGrade >= 90) {
+        console.log("A");
+    }else if (studentGrade >= 80) {
+        console.log("B");
+    }else if (studentGrade >= 70) {
+        console.log("C");
+    }else if (studentGrade >= 60) {
+        console.log("D");
+    }else {
+        console.log("F");
+    }
+}
+
+console.log(letterGrade(studentGrade: 88));
+
+
+
+
+
