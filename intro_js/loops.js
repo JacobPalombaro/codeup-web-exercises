8199e64f2d2c8d9f47da6154eadc489b55fde9de
Jacob Palombaro <jacob.palombaro@gmail.com>
2021-12-02 11:03:29 -0600

    update

---
+++ intro_js/loops.js
@@ -0,0 +1,61 @@
+//loops
+
+
+
+//---While--
+// 20 parking spots open
+// 1 car driving arund
+// ? how many times will have to drive around to find spot
+
+// while(conditions){
+//     //code here at every iteration
+// }
+//
+// while (car is unparked){
+//     keep driving
+// }
+
+let parkingSpots = 20;
+let aisles = 10;
+let movingCars = 1;
+// if(parkingSpots == 0)
+let isEmpty = false;
+
+// while(isEmpty == false){
+//     console.log(movingCars);
+//     movingCars++
+// }
+
+// let num = 2;
+// let age = 31;
+// console.log(num = num++);
+// console.log(age = ++age);
+
+
+
+let i = 20; // starting point
+while(i < 0){
+    console.log("iteration:", i);
+    // i++;
+    // i = i + 1;     // these all return same answer
+    ++i;
+}
+// 0 available spots
+// 20 parking spots (ending point)
+// loop until a parking spot opens up
+// iterating through the loop
+//console.log there re x num spots available;
+
+
+
+
+
+//--Do While--
+
+
+
+//--For Loops--
+
+
+
+//--
