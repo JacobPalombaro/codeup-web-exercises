ad9577970f7e8d7f045646407f3c94fd3e30187f
Jacob Palombaro <jacob.palombaro@gmail.com>
2021-12-03 11:25:32 -0600

    update

---
+++ intro_js/arrays.js
@@ -0,0 +1,73 @@
+//
+// //create an array with your top 5 favorite foods;
+// give eah of your food a rating 1 - 5
+
+// console.log(students);
+// console.log(students[3]);
+// console.log(students[5]);
+
+// let students = ["gerald", "mark", "mina", "jason", "justin", "justin"];
+//
+// //concatenate the last name to the first name
+// //access Justin's value
+// //re-assign tht value to equal justin + lastname
+// console.log(students[4]);
+// students[4] += " sebastian"
+// console.log(students[4]);
+
+
+// console.log(students.length)
+//
+//
+//
+// console.log(students.lastIndexOf())
+// let favFoods = [
+//     "pizza",
+//     "pasta",
+//     "mac and cheese",
+//     "chicken",
+//     "steak"
+// ]
+// console.log(favFoods[0] + " 1")
+// let favFoods= ["pizza", "pasta", "mac nd cheese", "chicken", "steak"];
+
+
+// let students = ["gerald", "mark", "Mina", "jason", "justin", "justin"];
+// let indexOfMina = students.indexOf("Mina");
+// console.log(indexOfMina);
+// let otherIndexOfMina = students.lastIndexOf("Mina")
+// console.log(otherIndexOfMina);
+//
+//
+// let favAnimal = "My favorite animal is frogs";
+// let laura = ['laura', 31, 'san antonio', 'mac', ['pizza', 'french fries', 'ice cream'], 2021, favAnimal]
+// let age = laura[1];
+// console.log(age);
+// let innerArr = laura[4];
+// // console.log(innerArr);
+// // console.log(innerArr[1])
+// // console.log(laura[4][1])
+// console.log(laura[6]);
+// console.log(laura)
+//
+// function desiredAge (num){
+//     return num - 10
+// }
+//
+// console.log(desiredAge(age));
+
+// // methods
+// // accessing elements from an array
+// // nameOfArray[position withing the array] arrays always start at zero
+//
+// let valIndexZero = students [0];
+// console.log(valIndexZero)
+
+
+
+
+
+// console.log(students.indexOf("Mina"));//2
+//
+// console.log(students.lastIndexOf("Mina"));// 2,
+// console.log(students.lastIndexOf())
