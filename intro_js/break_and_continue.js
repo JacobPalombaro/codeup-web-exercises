a8d52f975317acb5d3c44b8f008ec744ad7383ec
Jacob Palombaro <jacob.palombaro@gmail.com>
2021-12-03 09:45:23 -0600

    update

---
+++ intro_js/break_and_continue.js
@@ -0,0 +1,27 @@
+
+var userNumber = 0;
+do {
+    userNumber = Number(prompt("give me a odd number between 1 && 50!"));
+    if (userNumber < 1 || userNumber > 50) {
+        alert("Incorrect")
+    } else if (userNumber % 2 === 0){
+        alert("Not odd")
+    }else if (isNaN(userNumber)){
+        alert("Not a number")
+    }else {
+        alert("finally you got it")
+        break;
+    }
+
+} while(true)
+
+
+console.log("Number to skip is: " + userNumber)
+
+for (var i = 1; i <= 50; i+=2){
+    if(i === userNumber){
+        console.log("Yikes: skipping number: " + userNumber)
+        continue;
+    }
+    console.log("Here is and odd number: " + i)
+}
--- intro_js/for_loops.js
+++ intro_js/for_loops.js
@@ -1,72 +1,15 @@
-// function showMultiplicationTable(arg){
-//     i = 1
-//     for(let i = 1, i <= 10, i++)
-// }
-// num = num++
-// function showMultiplicationTable(number) {
-//     for(let i = 1, i <= 10, i++) {
-//         console.log(number + "x" + i "=" + number*i);
-//     }
-// }
-// showMultiplicationTable(7)
 
 
-// function showMultiplicationTable(number) {
-//     for(let i = 0; i <= 10; i++) {
-//         console.log(number + " x " + i + " = " + number*i)
-//     }
-// }
-// showMultiplicationTable(7)
-// function getRndInteger(min, max){
-//     return Math.floor(Math.random() * (max, min) ) + min;
-// }
-// console.log(getRndInteger(20,200))
-// function isEvenOrOdd(number){
-//     for(let i = 1; i <= 10; i++) {
-//         number = Math.floor(Math.random() * 181) + 20;
-//         if (number % 2 === 0) {
-//             console.log(number + " is even")
-//         } else {
-//             console.log(number + " is odd")
-//         }
-//     }
-//
-//     }
-// console.log(isEvenOrOdd(""))
+function showMultiplicationTable(x){
+    for(var i = 1; i <= 10; i++){
+        var answer = x * i;
 
-// for (let i = 0; i < 9; i++) {
-//     let number = 0;
-//     console.log((number + 1) + 1);
-// }
-//
-// function generatePyramid() {
-//     let totalNumberOfRow = 9;
-//     let output = '';
-//     for (let i = 1; i <= 9; i++){
-//
-//     }
-//     }
-// //2.
-// function showMultiplicationTable(number) {
-//     for(let i = 0; i <= 10; i++) {
-//         console.log(number + " x " + i + " = " + number*i)
-//
-//
-//     }
-// }
-//
-// showMultiplicationTable(7)
-//
-// 3.
-// // function isEvenOrOod(number) {
-// //
-// // function getRndInteger(min, max) {
-// //     return Math.floor(Math.random() * (max - min) ) + min;
-// // }
-// //
-// // console.log(getRndInteger(20,200))
-function isEvenOrOod(number) {
-    for (let i = 1; i <= 10; i++) {
+        console.log(x+ 'x' + i + '+' + answer);
+    }
+}
+showMultiplicationTable(7)
+
+for (let i = 1; i <= 10; i++) {
         number = Math.floor(Math.random() * 181) + 20;
         if (number % 2 === 0) {
             console.log(number + " is even")
@@ -74,27 +17,21 @@ function isEvenOrOod(number) {
             console.log(number + " is odd")
         }
     }
-}
-
-console.log(isEvenOrOod())
-
-//4.
-for (i = 1; i <= 9; i++) {
+    for (i = 1; i <= 9; i++) {
     var res = [];
     for (a = 1; a <= i; a++) {
         res.push(i);
     }
     console.log(res.join(""));
 }
-
-//5.
 function countDown(number){
     for(i = 1; i <= 20; i++){
 
         console.log(number - 5*i)
     }
 }
-
 console.log(countDown(105))
+
+
 
 
