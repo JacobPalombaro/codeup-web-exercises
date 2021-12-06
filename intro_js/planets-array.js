6830562c5bdef16dd02ce22b52d3cc98c1bb0a3d
Jacob Palombaro <jacob.palombaro@gmail.com>
2021-12-06 09:18:36 -0600

    update

--- intro_js/planets-array.js
+++ intro_js/planets-array.js
@@ -22,48 +22,49 @@
     // console.log('Adding "The Sun" to the beginning of the planets array.');
     // console.log(planets);
 let planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"];
+
 console.log(planets)
+
 console.log("before", planets);
-//
+
 planets.unshift("The Sun");
 
 console.log(planets)
-//
-//
+
 console.log('Adding "Pluto" to the end of the planets array.');
 
 console.log(planets);
 
 planets.push("Pluto");
-//
+
 console.log("after push", planets)
-//
+
 console.log('Removing "The Sun" from the beginning of the planets array.');
 
-console.log(planets);
+planets.shift()
 
-planets.shift("running shift() method");
-//
 console.log(planets)
 
 console.log('Removing "Pluto" from the end of the planets array.');
 
 console.log(planets);
 
-console.log("removing last index of");
-
 planets.pop();
-
-console.log("after pop", planets);
 
 console.log('Finding and logging the index of "Earth" in the planets array.');
 
+console.log("earth's index:" + planets.indexOf("earth"))
 
-console.log("Reversing the order of the planets array.");
 
+// console.log("Reversing the order of the planets array.");
+
+console.log("reversing array")
+planets.reverse
 console.log(planets);
 
 console.log("Sorting the planets array.");
+
+planets.sort
 
 console.log(planets);
 // })();
