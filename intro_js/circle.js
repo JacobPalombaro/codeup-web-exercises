c9945a9d9bbef99ef691b92dc5b89558f89f2bef
Jacob Palombaro <jacob.palombaro@gmail.com>
2021-12-07 14:33:54 -0600

    finished objects

--- intro_js/circle.js
+++ intro_js/circle.js
@@ -9,31 +9,43 @@
         radius: 3,
 
         getArea: function circle (radius) {
-            this.radius = radius;
-            this.area = function () {
-                return Math.PI * this.radius * this.radius;
-            };
-            this.perimeter = function () {
-                return 2 * Math.PI * this.radius;
-            };
-            var c = new circle(3);
-            console.log("area=", c.area().toFixed(2))
-            console.log("perimeter =", c.perimeter().toFixed(2));
+            // this.radius = radius;
+            // this.area = function () {
+            //     return Math.PI * this.radius * this.radius;
+            // };
+            // this.perimeter = function () {
+            //     return 2 * Math.PI * this.radius;
+            // };
+            // var c = new circle(3);
+            // console.log("area=", c.area().toFixed(2))
+            // console.log("perimeter =", c.perimeter().toFixed(2));
+
+           // Math.pow(this.radius, 2)
+
+            return Math.PI * (this.radius ** 2)
+        },
 
             // TODO: return the proper value
 
-            return Math.PI * Math.pow(this.radius, 2)
-        },
+
+
 
         logInfo: function (doRounding) {
             // TODO: complete this method.
 
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
+let areaValue;
 
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+if (doRounding) {
+    areaValue = Math.round(this.getArea())
+}else{
+    areaValue = this.getArea();
+}
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + this.getArea());
         }
     };
+    console.log("Circle area", circle.getArea());
 
     // log info about the circle
     console.log("Raw circle information");
@@ -43,7 +55,7 @@
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
-
+circle.radius = 5;
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
