ed9599faf3df31d89cef3eed35d768fd75117c4c
Jacob Palombaro <jacob.palombaro@gmail.com>
2022-01-11 14:20:32 -0600

    update

--- intro_js/BOM.js
+++ intro_js/BOM.js
@@ -1,20 +1,56 @@
 'use strict';
 // BOM intro
-let sirius = {
-    numStudents: 30,
-    classroom: "Pedernales/remote",
-    // methods - functions stored inside of an object
-    takeAttendance: function(){
+// let sirius = {
+//     numStudents: 30,
+//     classroom: "Pedernales/remote",
+//     // methods - functions stored inside of an object
+//     takeAttendance: function(){
+//
+//         prompt("How many students?");
+//
+//         return this.numStudents;
+//     },
+//     prompt: function(){
+//     //returns prompt window;
+//     }
+// }
+// sirius.takeAttendance();
+var count = 0;
+var max = 10;
+var interval = 1000;
 
-        prompt("How many students?");
 
-        return this.numStudents;
-    },
-    prompt: function(){
-    //returns prompt window;
-    }
-}
-sirius.takeAttendance();
+var intervalId =
+    setInterval(
+        function(){
+            if (count >= max){
+                clearInterval(intervalId);
+                console.log('All done');
+            }else{
+                count++;
+                console.log("Repeating this line" + count);
+            }
+        },
+    interval);
+
+// set timeout
+var delay = 5000;
+
+var timeoutId = setTimeout( function (){
+    alert('Here is a delayed hello!');
+}, delay);
+
+window.location = 'https://java.codeup.com';
+var delay = 5000;
+
+var timeoutId = setTimeout(function (){
+    return window.location = "https://java.codeup.com";
+}, delay);
+
+
+
+
+
 
 //System Dialogue
 
