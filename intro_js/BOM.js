a42f66e1acf38c77738732add51edceea2060c68
Jacob Palombaro <jacob.palombaro@gmail.com>
2022-01-11 11:39:32 -0600

    update

---
+++ intro_js/BOM.js
@@ -0,0 +1,26 @@
+'use strict';
+// BOM intro
+let sirius = {
+    numStudents: 30,
+    classroom: "Pedernales/remote",
+    // methods - functions stored inside of an object
+    takeAttendance: function(){
+
+        prompt("How many students?");
+
+        return this.numStudents;
+    },
+    prompt: function(){
+    //returns prompt window;
+    }
+}
+sirius.takeAttendance();
+
+//System Dialogue
+
+
+//Set Interval
+
+
+// Set Timeout
+
