3d499fee026dafae2f599c40844c62d8add620b1
Jacob Palombaro <jacob.palombaro@gmail.com>
2022-01-12 10:00:23 -0600

    update

--- intro_js/BOM.js
+++ intro_js/BOM.js
@@ -1,62 +1,73 @@
-'use strict';
-// BOM intro
-// let sirius = {
-//     numStudents: 30,
-//     classroom: "Pedernales/remote",
-//     // methods - functions stored inside of an object
-//     takeAttendance: function(){
+// 'use strict';
+// // BOM intro
+// // let sirius = {
+// //     numStudents: 30,
+// //     classroom: "Pedernales/remote",
+// //     // methods - functions stored inside of an object
+// //     takeAttendance: function(){
+// //
+// //         prompt("How many students?");
+// //
+// //         return this.numStudents;
+// //     },
+// //     prompt: function(){
+// //     //returns prompt window;
+// //     }
+// // }
+// // sirius.takeAttendance();
+// var count = 0;
+// var max = 10;
+// var interval = 1000;
 //
-//         prompt("How many students?");
 //
-//         return this.numStudents;
-//     },
-//     prompt: function(){
-//     //returns prompt window;
-//     }
-// }
-// sirius.takeAttendance();
-var count = 0;
-var max = 10;
-var interval = 1000;
+// var intervalId =
+//     setInterval(
+//         function(){
+//             if (count >= max){
+//                 clearInterval(intervalId);
+//                 console.log('All done');
+//             }else{
+//                 count++;
+//                 console.log("Repeating this line" + count);
+//             }
+//         },
+//     interval);
+//
+// // set timeout
+// var delay = 5000;
+//
+// var timeoutId = setTimeout( function (){
+//     alert('Here is a delayed hello!');
+// }, delay);
+//
+// window.location = 'https://java.codeup.com';
+// var delay = 5000;
+//
+// var timeoutId = setTimeout(function (){
+//     return window.location = "https://java.codeup.com";
+// }, delay);
+//
+//
+//
+//
+//
+//
+// //System Dialogue
+//
+//
+// //Set Interval
+//
+//
+// // Set Timeout
 
+var promptAnswer = prompt("whats your name?");
 
-var intervalId =
-    setInterval(
-        function(){
-            if (count >= max){
-                clearInterval(intervalId);
-                console.log('All done');
-            }else{
-                count++;
-                console.log("Repeating this line" + count);
-            }
-        },
-    interval);
+var retrievedSpan = document.getElementById("name");
+console.log("promptAnswer: ", promptAnswer);
+console.log("retrievedSpan: ", retrievedSpan);
 
-// set timeout
-var delay = 5000;
+retrievedSpan.innerHTML = promptAnswer;
 
-var timeoutId = setTimeout( function (){
-    alert('Here is a delayed hello!');
-}, delay);
-
-window.location = 'https://java.codeup.com';
-var delay = 5000;
-
-var timeoutId = setTimeout(function (){
-    return window.location = "https://java.codeup.com";
-}, delay);
-
-
-
-
-
-
-//System Dialogue
-
-
-//Set Interval
-
-
-// Set Timeout
+// --
+// document --> everything in the body of your HTML
 
